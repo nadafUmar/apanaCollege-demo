3a42a0575dc9a096c9ec78c35bdb2d78619c65bb
nadafUmar <nadafmulla97@gmail.com>
2024-03-09 20:37:21 +0545

    java program to add two numbers

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # apanaCollege-demo
 this is my first git repository
+author-> umar nadaf
---
+++ addTwoNum.java
@@ -0,0 +1,8 @@
+public class addTwoNum{
+    public static void main(String[] args) {
+        int a =90;
+        int b =80;
+        int sum=a+b;
+        System.out.println("sum is :"+sum);
+    }
+}
